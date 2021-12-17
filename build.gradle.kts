dbba22ed6a8e5f61d092881d1a374b39074f0ca2
Justin Brooks <justin.brooks@faithlife.com>
2021-12-17 14:51:01 -0600

    Update build tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.android.build.gradle.internal.tasks.factory.dependsOn
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 plugins {
-    kotlin("multiplatform") version "1.6.0"
+    kotlin("multiplatform") version "1.6.10"
     id("org.jetbrains.changelog") version "1.3.0"
     id("com.vanniktech.maven.publish") version "0.18.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
